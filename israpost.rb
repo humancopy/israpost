2ec6b76baf3f9349789347ba1ce1966ecf19032e
humancopy <info@humancopy.net>
2015-01-05 14:33:17 +0100

    small fixes

--- israpost.rb
+++ israpost.rb
@@ -1,11 +1,14 @@
 require_relative 'post_rate'
 
-# SHOP_NAME = 'symbolika'
-# API_KEY   = 'e7e376621828b493967d7eb86e1cc5ba'
-# PASSWORD  = '87531ea3fed8cf32bd8bcdd526c65176'
-SHOP_NAME = 'symbolika-dev'
-API_KEY  = 'c3300985faee6c64562e5c46406a3a8f'
-PASSWORD = '79b6b3e1c31eb1868fcfbeece019c265'
+if ENV['RACK_ENV'] == 'production'
+  SHOP_NAME = 'symbolika'
+  API_KEY   = 'e7e376621828b493967d7eb86e1cc5ba'
+  PASSWORD  = '87531ea3fed8cf32bd8bcdd526c65176'
+else
+  SHOP_NAME = 'symbolika-dev'
+  API_KEY   = 'c3300985faee6c64562e5c46406a3a8f'
+  PASSWORD  = '79b6b3e1c31eb1868fcfbeece019c265'
+end
 
 HANDLING_PRICE = 12 # in ILS
 CURRENCY_CODE  = 'EUR'
@@ -21,7 +24,7 @@ helpers do
 end
 
 get '/' do
-  "Goto https://github.com/mjnissim/israpost for more info."
+  "Goto https://github.com/humancopy/shopify-israpost for more info."
 end
 
 post '/rates' do
@@ -33,7 +36,7 @@ post '/rates' do
 
   json :rates => [
       {
-        service_name: "7-21 days registered mail",
+        service_name: "registered mail",
         service_code: "AIR",
         total_price: price(rates['airmail']),
         currency: CURRENCY_CODE,
@@ -41,7 +44,7 @@ post '/rates' do
         max_delivery_date: DateTime.now + 21.days
       },
       {
-        service_name: "3-5 days speed post",
+        service_name: "speed post",
         service_code: "EMS",
         total_price: price(rates['ems']),
         currency: CURRENCY_CODE,
@@ -57,25 +60,14 @@ get '/create' do
 
   ShopifyAPI::CarrierService.all.collect(&:destroy)
 
-  success  = 0
-  services = [
+  [
     {
       name: 'israeli post',
-      callback_url: 'http://shopify-israpost.heroku.com/rates',
+      callback_url: 'https://shopify-israpost.herokuapp.com/rates',
       service_discovery: true,
       format: 'json'
     }
-  ]
-  services.each do |service|
-    new_service = ShopifyAPI::CarrierService.create(service)
-    new_service.name              = service[:name]
-    new_service.callback_url      = service[:callback_url]
-    new_service.service_discovery = service[:service_discovery]
-    new_service.format            = service[:format]
-    logger.info "new_service = #{new_service.inspect}"
-    success = success + 1 if new_service.save
-  end
-  success == services.count ? "done ;)" : "problem :("
+  ].select { |service| !ShopifyAPI::CarrierService.create(service) }.any? ? "problem :(" : "done ;)"
 end
 
 error do
