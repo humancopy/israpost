16c405f3df7de1cd8d5b06bda0be05bacf896fd3
humancopy <info@humancopy.net>
2017-03-29 14:08:31 +0200

    support different app names

--- israpost.rb
+++ israpost.rb
@@ -226,8 +226,8 @@ class Israpost < Sinatra::Application
 
     [
       {
-        name: 'israeli post',
-        callback_url: "https://shopify-israpost#{'-dev' if ENV['RACK_ENV'] == 'staging'}.herokuapp.com/rates",
+        name: ENV['APP_NAME'].gsub('-', ' ').split.map(&:capitalize).join(' '),
+        callback_url: "https://#{ENV['APP_NAME']}.herokuapp.com/rates",
         service_discovery: true,
         carrier_service_type: 'api',
         format: 'json'
